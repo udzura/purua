ee91a997dcdbdf2f4f08f94f5389d3f0fcf3e3dc
Uchio Kondo <udzura@udzura.jp>
2021-04-30 14:18:49 +0900

    Sample

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ use std::env;
 use combine::parser::char::{char, digit, space, spaces};
 use combine::stream::position;
 
-use combine::{many1, skip_many, EasyParser, ParseError, Parser, Stream};
+use combine::{chainl1, many1, skip_many, EasyParser, ParseError, Parser, Stream};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum Expr {
     Number(i32),
     Plus(Box<Expr>, Box<Expr>),
+    Minus(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
 }
 
 fn number<Input>() -> impl Parser<Input, Output = Box<Expr>>
@@ -18,7 +21,27 @@ where
     Input: Stream<Token = char>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    many1(digit()).map(|d: String| Box::new(Expr::Number(d.parse::<i32>().unwrap())))
+    many1(digit())
+        .skip(spaces())
+        .map(|d: String| Box::new(Expr::Number(d.parse::<i32>().unwrap())))
+}
+
+fn muldiv<Input>() -> impl Parser<Input, Output = Box<Expr>>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    let token = char('*').or(char('/')).skip(spaces()).map(|tok| {
+        move |d1, d2| {
+            if tok == '*' {
+                Box::new(Expr::Mul(d1, d2))
+            } else {
+                Box::new(Expr::Div(d1, d2))
+            }
+        }
+    });
+
+    chainl1(number(), token)
 }
 
 fn binop<Input>() -> impl Parser<Input, Output = Box<Expr>>
@@ -26,20 +49,25 @@ where
     Input: Stream<Token = char>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    (
-        number(),
-        skip_many(space()),
-        char('+').or(char('-')),
-        skip_many(space()),
-        number(),
-    )
-        .map(|(d1, _, tok, _, d2)| {
+    let token = char('+').or(char('-')).skip(spaces()).map(|tok| {
+        move |d1, d2| {
             if tok == '+' {
                 Box::new(Expr::Plus(d1, d2))
             } else {
-                unreachable!("TODO")
+                Box::new(Expr::Minus(d1, d2))
             }
-        })
+        }
+    });
+
+    chainl1(muldiv(), token)
+}
+
+fn myparser<Input>() -> impl Parser<Input, Output = Box<Expr>>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    (spaces(), binop()).map(|(_, res)| res)
 }
 
 fn main() {
@@ -51,7 +79,7 @@ fn main() {
 }
 
 fn do_main<'a>(text: &'a str) -> Result<(), Box<dyn std::error::Error + 'a>> {
-    let mut parser = (spaces(), binop(), spaces()).map(|(_, res, _)| res.clone());
+    let mut parser = myparser();
 
     let pos = position::Stream::new(text);
     let res = parser.easy_parse(pos)?.0;
